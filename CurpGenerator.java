a5d911f85456e92500b8f17efa96a80585e29d5a
Carlos Montes <carloss.montes@gmail.com>
2024-03-14 12:17:30 -0600

    Push inicial

---
+++ CurpGenerator.java
@@ -0,0 +1,3 @@
+public class CurpGenerator {
+    public String mensajeBienvenida = "Este es solo un mensaje de bienvenida para el nuevo proyecto en git";
+}
---
+++ CurpGeneratorTest.java
@@ -0,0 +1,6 @@
+public class CurpGeneratorTest {
+    public static void main(String[] args) {
+        String os = "Ubuntu 22.04", javaVersion = "openjdk11", ide = "Visual Studio Code";
+        System.out.println("Hello, World for "+os+" and "+javaVersion+" with "+ide);
+    }
+}
