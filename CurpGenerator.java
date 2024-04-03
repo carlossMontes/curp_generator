c34bde7c4ca869717d871901705f3ad50dbd6398
Carlos Montes <carloss.montes@gmail.com>
2024-04-02 19:51:24 -0600

    pidiendo los datos

--- CurpGenerator.java
+++ CurpGenerator.java
@@ -1,3 +1,46 @@
 public class CurpGenerator {
-    public String mensajeBienvenida = "Este es solo un mensaje de bienvenida para el nuevo proyecto en git";
+    public String pat;
+    public String mat;
+    public String nombre;
+    public String nac;
+    public String genero;
+    public String edo;
+
+    public String getPat() {
+        return pat;
+    }
+    public void setPat(String pat) {
+        this.pat = pat;
+    }
+    public String getMat() {
+        return mat;
+    }
+    public void setMat(String mat) {
+        this.mat = mat;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getNac() {
+        return nac;
+    }
+    public void setNac(String nac) {
+        this.nac = nac;
+    }
+    public String getGenero() {
+        return genero;
+    }
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    public String getEdo() {
+        return edo;
+    }
+    public void setEdo(String edo) {
+        this.edo = edo;
+    }
+
 }
