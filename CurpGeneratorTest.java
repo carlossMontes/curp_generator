af968f002cd23751ac251802c973a0d1c3e633a5
Carlos Montes <carloss.montes@gmail.com>
2024-04-02 19:55:51 -0600

    Pidiendo datos

--- CurpGeneratorTest.java
+++ CurpGeneratorTest.java
@@ -2,7 +2,28 @@ import java.util.Scanner;
 
 public class CurpGeneratorTest {
     public static void main(String[] args) {
-        String os = "Ubuntu 22.04", javaVersion = "openjdk11", ide = "Visual Studio Code";
-        System.out.println("Hello, World for "+os+" and "+javaVersion+" with "+ide);
+
+        Scanner sc = new Scanner(System.in);
+        CurpGenerator cg = new CurpGenerator();
+
+        System.out.println("=== Generador de CURP ===\n Ingrese el apellido paterno");
+        cg.setPat(sc.nextLine());
+
+        System.out.println("Ingrese el apellido materno");
+        cg.setMat(sc.nextLine());
+
+        System.out.println("Ingrese el nombre");
+        cg.setNombre(sc.nextLine());
+
+        System.out.println("Ingrese la fecha de nacimiento");
+        cg.setNac(sc.nextLine());
+
+        System.out.println("Ingrese el genero");
+        cg.setGenero(sc.nextLine());
+
+        System.out.println("Ingrese el estado de nacimiento");
+        cg.setEdo(sc.nextLine());
+
+        sc.close();
     }
 }
